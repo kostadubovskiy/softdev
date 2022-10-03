d160df260aeb0dafdc0cd1e3f18e7b27eb1a47a0
kostadubovskiy <kostadubovskiy@gmail.com>
2022-10-03 09:23:43 -0400

    07

--- 06_py-csv/numbercruncher.py
+++ 06_py-csv/numbercruncher.py
@@ -8,6 +8,7 @@ Soft Dev
 '''
 DISCO:
 *Global/local variable scope in python
+*fileinput
 QCC:
 
 '''
@@ -31,17 +32,6 @@ def read():
     
     return curr[:-1] + [['Other', 0.2]]
     
-# print(f"{len(read())}, {lines}")
-
-# sum = 0
-# for line in read():
-#     print(line)
-#     try:
-#         sum += line[1]
-#     except:
-#         pass
-# 
-# print(sum)
 def make_dict(l):
     result = {}
     for line in l:
@@ -52,20 +42,20 @@ def weighted_select(d):
     rnum = random.random() * 100
     print(rnum)
     running_val_sum = 0
-#     keys = list(d.keys())
     line = 0
     for k in d:
         if line != 0:
             running_val_sum += d[k]
         if running_val_sum >= rnum:
             return k
-        line += 1
-#return 
+        line += 1 
 d1 = {
     "hi" : 50,
     "hello": 30,
     "wassup": 20
     }
-print(weighted_select(d1))
+# print(weighted_select(d1))
 # final test
+print(f"read: {read()}")
+print(f"dict: {make_dict(read())}")
 print(weighted_select(make_dict(read())))
