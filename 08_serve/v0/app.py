b7aa4791b1a7b9b2a31667e847483b5f7c77270b
kostadubovskiy <kostadubovskiy@gmail.com>
2022-10-10 18:16:52 -0400

    08

--- 08_serve/v0/app.py
+++ 08_serve/v0/app.py
@@ -1,13 +1,14 @@
+
 # Clyde 'Thluffy' Sinclair
 # SoftDev
 # Oct 2022
 
 from flask import Flask
-app = Flask(__name__) # creates instance of class Flask
+app = Flask(__name__) # ...
 
-@app.route("/") # routes to root directory
+@app.route("/") # ...
 def hello_world():
-    print(__name__) # prints to console
-    return "No hablo queso!"  # prints to created server
+    print(__name__) # ...
+    return "No hablo queso!"  # ...
 
-app.run()  # creates web server
+app.run()  # ...
--- 08_serve/v2/app.py
+++ 08_serve/v2/app.py
@@ -8,7 +8,7 @@ app = Flask(__name__) #create instance of class Flask
 @app.route("/")       #assign fxn to route
 def hello_world():
     print("about to print __name__...")
-    print(__name__)   # prints __main__ to the console
+    print(__name__)   #where will this go?
     return "No hablo queso!"
 
 app.run()
