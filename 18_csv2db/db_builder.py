afbe47ad06ccf9ccd09c9653468f8c62efa9caab
kostadubovskiy <kostadubovskiy@gmail.com>
2022-10-27 09:22:47 -0400

    p00

--- 18_csv2db/db_builder.py
+++ 18_csv2db/db_builder.py
@@ -1,46 +1,51 @@
-#Clyde "Thluffy" Sinclair
-#SoftDev  
-#skeleton/stub :: SQLITE3 BASICS
-#Oct 2022
-
 import sqlite3   #enable control of an sqlite database
 import csv       #facilitate CSV I/O
 import os
 
-os.system('rm -rf discobandit.db')
-DB_FILE="discobandit.db"
+os.system('rm -rf discobandit.db') # this is to delete the dataframe file as to not encounter any errors during testing
 
-db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
-c = db.cursor()   
 
-filename = 'students.csv'
+def populate_table(file_name,table_name,elements,keys):
+
+    db = sqlite3.connect(file_name) #open if file exists, otherwise create
+    c = db.cursor() # creates cursor object to pass commands to the database
+
+    c.execute(f'''create table if not exists {table_name}('{keys[0]}' text, {keys[1]} int, {keys[2]} text);''') # creates the table
+    for row in elements:
+
+        # iterate through the elements and add them to the table ( row by row )
+        c.execute(f'''insert into {table_name} values('{row[keys[0]]}',{row[keys[1]]},'{row[keys[2]]}');''') 
+
+    c.execute(f'''select * from {table_name}''') # for some reason need this execute statment to get fetchall
+    return_msg = c.fetchall() # returns a list of tuples that are rows
+
+    db.commit() #save changes
+    db.close()  #close database
+        
+    return (return_msg)
+
+filename = '/home/students/2023/kdubovskiy30/sd/softdev/18_csv2db/students.csv'
 with open(filename,newline='') as csvfile:
-
-    reader = csv.DictReader(csvfile)
-    csv_list = list(reader)
-    keys = list((csv_list[0]).keys())
-
-
-          #facilitate db ops -- you will use cursor to trigger db events
-
-#==========================================================
+    student_reader = csv.DictReader(csvfile)
+    student_csv_list = list(student_reader)
+    student_keys = list((student_csv_list[0]).keys())
 
 
-# < < < INSERT YOUR TEAM'S POPULATE-THE-DB CODE HERE > > >
-c.execute(f'''create table students('{str(keys[0])}' text, '{str(keys[1])}' int, {keys[2]} text)''')
-for row in csv_list:
-    # c.execute(f'''insert into students({row[keys[0]]},{row[keys[1]]},{row[keys[2]]})''')
-    c.execute(f'''insert into students values('andrew',199,'9000')''')
-    
-    
-# c.execute('''.import --csv --skip 1 --schema temp /home/students/2023/kdubovskiy30/sd/softdev/18_csv2db/students.csv students''')
+DB_FILE="discobandit.db"
+student_info = populate_table(DB_FILE,'students',student_csv_list,student_keys)
+print(student_info)
 
-command = "select * from students"          # test SQL stmt in sqlite3 shell, save as string
-c.execute(command)    # run SQL statement
 
-#==========================================================
+filename = '/home/students/2023/kdubovskiy30/sd/softdev/18_csv2db/courses.csv'
+with open(filename,newline='') as csvfile:
+    course_reader = csv.DictReader(csvfile)
+    course_csv_list = list(course_reader)
+    course_keys = list((course_csv_list[0]).keys())
 
-db.commit() #save changes
-db.close()  #close database
+
+course_info = populate_table(DB_FILE,'courses',course_csv_list,course_keys) # open up the same database, and then make a new table
+print(course_info)
+
+
 
 
