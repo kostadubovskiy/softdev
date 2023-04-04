0fff8bb4613159def2ed4556c2222e1396cde120
Kosta Dubovskiy <kosta@MacBook-Pro-5.local>
2023-04-04 09:26:20 -0400

    js

--- 27_js/fibfac.js
+++ 27_js/fibfac.js
@@ -11,3 +11,32 @@
 
 
 //Do whatever you think is needed. Think: S I M P L E.   Think: S M A R T.
+
+// fact
+function fact(n) {
+    if (n < 2){
+        return 1
+    } else{
+        return n * fact(n - 1)
+    }
+}
+
+console.log(fact(1))
+console.log(fact(2))
+console.log(fact(3))
+console.log(fact(4))
+console.log(fact(5))
+
+function fib(n) {
+    if (n < 2){
+        return 1
+    } else{
+        return fib(n - 1) + fib(n - 2)
+    }
+}
+
+console.log(fib(1))
+console.log(fib(2))
+console.log(fib(3))
+console.log(fib(4))
+console.log(fib(5))
